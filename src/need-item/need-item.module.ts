408b078c0a489120f3275b75c89903c098d6edd7
Luciana Kyoko <lukyoko@protonmail.com>
2025-04-03 13:22:29 -0300

    feat(need-item): add guards to protect need-item endpoints

--- src/need-item/need-item.module.ts
+++ src/need-item/need-item.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { NeedItemService } from './need-item.service';
 import { NeedItemController } from './need-item.controller';
 import { AnimalModule } from '../animal/animal.module';
+import { AuthModule } from '../auth/auth.module';
+import { AuthGuard } from '../auth/guard/auth.guard';
 
 @Module({
-  imports: [AnimalModule],
-  providers: [NeedItemService],
+  imports: [AnimalModule, AuthModule],
+  providers: [NeedItemService, AuthGuard],
   controllers: [NeedItemController],
 })
 export class NeedItemModule {}
