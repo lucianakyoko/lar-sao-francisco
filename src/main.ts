dd3063199c13c4e0354c30e911e102aaaed13382
Luciana Kyoko <lukyoko@protonmail.com>
2025-04-03 14:59:56 -0300

    feat(main): configure the CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://lar-sao-francisco.vercel.app',
+    methods: 'GET,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT ?? 3000);
