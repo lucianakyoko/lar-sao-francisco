f62e75b27668cfaa8573cf1ebac56d30d82d6299
Luciana Kyoko <lukyoko@protonmail.com>
2025-04-26 18:52:22 -0300

    .

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,30 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto.username, loginDto.password);
+  async login(
+    @Body() loginDto: LoginDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const { access_token } = await this.authService.login(
+      loginDto.username,
+      loginDto.password,
+    );
+
+    response.cookie('token', access_token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
+      path: '/dashboard',
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    return { message: 'Login successful' };
   }
 }
